a42eac0fab4d36bf3a1719eecfc101f17a7a8444
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-05-31 19:42:37 +0300

    to upper

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for i in str:
+        i = ord(i)
+        if i >= 97 and i <= 123:
+            i = i - 32
+            i = chr(i)
+        else:
+            i = chr(i)
+        print("{:s}".format(i), end='')
+    print('')
