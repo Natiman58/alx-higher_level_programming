eff9efd49065b774783196223313ac30721a973b
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-05-31 13:12:59 +0300

    added space

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 10):
     for j in range(0, 10):
         if i < j and i == 8 and j == 9:
-            print("{:d}{:d}".format(i, j), end="")
+            print("{:d}{:d}".format(i, j), end=" ")
         elif i < j:
             print("{:d}{:d}".format(i, j), end=", ")
