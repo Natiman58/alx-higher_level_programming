8dd4b274633255ecee7bdfadf60675035cd961a8
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-05-31 13:11:07 +0300

    added format

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for i in range(0, 10):
     for j in range(0, 10):
         if i < j and i == 8 and j == 9:
-            print(f"{i}{j}", end="")
+            print("{:d}{:d}".format(i, j), end="")
         elif i < j:
-            print(f"{i}{j}", end=", ")
+            print("{:d}{:d}".format(i, j), end=", ")
