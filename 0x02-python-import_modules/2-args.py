03bdcdbd132fea88f6ec37df8502a9e622aafc4f
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-06-02 13:56:47 +0300

    imported not

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-import sys
-arg_v = sys.argv[1:]
-arg_c = len(arg_v)
-i = 1
-if arg_c == 0:
-    print(f"{arg_c} arguments.")
-elif arg_c == 1:
-    print(f"{arg_c} argument:")
-    print(f"{arg_c}:", sys.argv[1])
-else:
-    print(f"{arg_c} arguments:")
-    while i <= arg_c:
-        print(f"{i}: {sys.argv[i]}")
-        i += 1
+if __name__ == "__main__":
+    import sys
+    arg_v = sys.argv[1:]
+    arg_c = len(arg_v)
+    i = 1
+    if arg_c == 0:
+        print(f"{arg_c} arguments.")
+    elif arg_c == 1:
+        print(f"{arg_c} argument:")
+        print(f"{arg_c}:", sys.argv[1])
+    else:
+        print(f"{arg_c} arguments:")
+        while i <= arg_c:
+            print(f"{i}: {sys.argv[i]}")
+            i += 1
