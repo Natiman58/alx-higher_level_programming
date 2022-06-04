3d29fdb65fb938dea08dabc541cdd8e8f745dd1d
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-06-04 22:07:33 +0300

    fixed return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    new_tuple = ()
     if len(sentence) == 0:
-        new_tuple[0] = "None"
+        sentence[0] = "None"
     else:
         length = len(sentence)
         first_char = sentence[0]
         new_tuple = (length, first_char)
-        return new_tuple
+    return new_tuple
