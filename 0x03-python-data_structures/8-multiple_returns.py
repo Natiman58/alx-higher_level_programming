8aa81a05bdc2ba2a94763a674789913ec5a1ab04
Nati M <99422296+Natiman58@users.noreply.github.com>
2022-06-04 21:40:43 +0300

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,7 @@ def multiple_returns(sentence):
     """Write a function that returns a tuple with\
              the length of a string and its first character."""
     if len(sentence) == 0:
-        return None
+        sentence[0] = None
     else:
         length = len(sentence)
         first_char = sentence[0]
