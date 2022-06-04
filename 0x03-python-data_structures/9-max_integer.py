9b6204b7ae4c757a4e8d87577a87ada07e6c401f
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-06-04 22:44:39 +0300

    len

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if my_list == 0:
+    if len(my_list) == 0:
         return None
     for i in range(len(my_list)):
         if my_list[i + 1] > my_list[i]:
