387f401cadc50c6e03de91d58b822a807cb29d6c
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-06-08 22:02:41 +0300

    roman

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not roman_string or None:
+        return 0
+    roman_numerals = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    result = 0
+    for i,c in enumerate(roman_string):
+        if (i + 1) == len(roman_string) or roman_numerals[c] >= roman_numerals[roman_string[i + 1]]:
+            result += roman_numerals[c]
+        else:
+            result -= roman_numerals[c]
+    return result
+
