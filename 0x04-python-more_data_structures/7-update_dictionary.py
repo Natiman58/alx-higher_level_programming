af1246f55b54e554a3643bb67703772145244141
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-06-08 16:50:33 +0300

    update

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+
+    if key in a_dictionary:
+        for i in a_dictionary:
+            if i == key:
+                a_dictionary[i] = value
+    a_dictionary[key] = value
+    return (a_dictionary)
