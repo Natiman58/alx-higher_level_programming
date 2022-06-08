8495d6bd85f675f1fc8a639aaebfe4c2eead87b4
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-06-08 22:26:07 +0300

    py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if not roman_string or None:
+    if not roman_string or type(roman_string) != str:
         return 0
     r_num = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
