cf56e70b6b268afc37e380e42983900798a54227
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-06-20 16:42:46 +0300

    stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
+    import sys
     try:
         print("{:d}".format(value))
         return True
     except Exception as exe:
-        print("Exception:", exe)
+        print("Exception: {}".format(exe), file=sys.stderr)
         return False
