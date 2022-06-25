3770425f562598603a5e021b8ebc68d5b52a02a2
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-06-25 11:55:39 +0300

    #

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """
-    a function that prints My name is <first name> <last name>
+    a function that prints My name
 """
 
 
 def say_my_name(first_name, last_name=""):
+    """
+    a function that prints My name is <first name> <last name>
+    """
     if not isinstance(first_name, str):
         raise TypeError('first_name must be a string')
     if not isinstance(last_name, str):
---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+     a function that prints\
+     a square with the character #
+"""
+
+
+def print_square(size):
+    """
+     a function that prints\
+     a square with the character # of size value times.
+    """
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    if isinstance(size, float) and size < 0:
+        raise TypeError('size must be an integer')
+    for i in range(size):
+        print('#' * size)
+
