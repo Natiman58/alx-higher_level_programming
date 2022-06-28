6ecaa3386bbbe143ad7bb617a9ee7a84dcb5b2a6
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-06-28 17:51:06 +0300

    no __init__

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -7,7 +7,3 @@ except if the new instance attribute is called first_name.
 
 class LockedClass:
     __slots__ = ['first_name']
-
-    def __init__(self):
-        """Initialize an empty locked class"""
-        pass
