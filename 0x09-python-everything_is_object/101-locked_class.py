8b2f9ac897992d1bc929a125f54b3f7099210279
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-06-28 17:52:19 +0300

    locked

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 """
-A class that prevents the user from dynamically creating new instance attributes,\
-except if the new instance attribute is called first_name.
+    A class that prevents the user from\
+    dynamically creating new instance attributes,\
+    except if the new instance attribute is called first_name.
 """
 
 
