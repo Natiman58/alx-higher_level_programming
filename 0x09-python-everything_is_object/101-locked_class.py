e38f23257c01c7f961dff7d96ec30552c241e13e
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-06-29 07:10:42 +0300

    documented class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """
     A class that prevents the user from\
-    dynamically creating new instance attributes,\
-    except if the new instance attribute is called first_name.
+    dynamically creating new instance attributes
 """
 
 
 class LockedClass:
+    """
+    A class that prevents the user from\
+    dynamically creating new instance attributes,\
+    except if the new instance attribute is called first_name.
+    """
     __slots__ = ['first_name']
 
     def __init__(self):
