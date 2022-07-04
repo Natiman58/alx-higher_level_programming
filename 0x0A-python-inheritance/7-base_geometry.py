7c8ddd7534f0f61ef8d12211a8d2faf32b3201ac
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-07-04 18:51:18 +0300

    l

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+    A module containing a class called BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """A class with public instance methods"""
+    def area(self):
+        """Raise an exception"""
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        """Validates if a name has the proper value"""
+        if type(value) is not int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
