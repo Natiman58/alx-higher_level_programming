0efc4dec1cbf1aed716f6b19d4736fc3ceadb107
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-07-04 20:51:57 +0300

    rebel

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+    A method that holds an inverted class\
+    that inherits from int super class\
+    and returns the opposite;\
+    equal is interpreted as not equal\
+    and vice versa.
+"""
+
+
+class MyInt(int):
+    """A class that inverts results."""
+    def __eq__(self, value):
+        """A magic method that is called by default\
+        when we use '==' sign and returns the opposite"""
+        return super().__ne__(value)
+
+    def __ne__(self, value):
+        """
+        A magic method that is called by default\
+        when we use '!=' sign and returns the opposite.
+        """
+        return super().__eq__(value)
