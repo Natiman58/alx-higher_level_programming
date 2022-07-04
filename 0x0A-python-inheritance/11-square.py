3eb71dc1e7dfe2f9895e34aad6e6ad5c125faf8d
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-07-04 20:30:17 +0300

    #2

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+A method that creates a square by inheritance.
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    A class called square that passes size 2X hence width\
+    and height are same for a square
+    """
+
+    def __init__(self, size):
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Method to calculate area of the square"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Returns the string format of the square."""
+        return "[Square] {}/{}".format(self.__size, self.__size)
