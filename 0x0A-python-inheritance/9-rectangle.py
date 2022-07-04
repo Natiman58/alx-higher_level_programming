a80142606dbc7c48ff3af7195e7b06d9770a16c4
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-07-04 19:55:09 +0300

    full

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle data inherits from BaseGeometry
+    """
+
+    def __init__(self, width, height):
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """Returns the area of the rectangle"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """A magic method that returns string format of Rectangle"""
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
