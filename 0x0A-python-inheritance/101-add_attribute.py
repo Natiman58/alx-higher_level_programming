27be8f3265242bb62bb5593f2b95a5f637dbd5db
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-07-04 21:59:02 +0300

    str

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python3
 """
-    A module that
+    A module that adds an attribute to an obj if possible.
 """
 
 
+def add_attribute(oj_name, at_name, at_value):
+    """
+    a method that adds an attribute to an obj.\
+    'oj_name' - object name\
+    'at_name' - attribute name\
+    'at_value' - attribute value we want to set
+    """
+    set_t = {str}
+
+    if type(oj_name) in set_t:
+        raise TypeError("Can't add new attribute")
+    else:
+        oj_name.__setattr__(at_name, at_value)
