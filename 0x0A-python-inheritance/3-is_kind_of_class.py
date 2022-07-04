3a4745322c2ac6f69f8bc6e0fff073a66e337173
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-07-04 14:12:42 +0300

    where

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -11,7 +11,5 @@ def is_kind_of_class(obj, a_class):
     or if the object is an instance of a class that\
     inherited from, the specified class ; otherwise False.
     """
-    if isinstance(obj, a_class):
+    if type(obj) is not a_class:
         return True
-    else:
-        return False
---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A module to check where an obj is inherited from
+"""
+
+
+def inherits_from(obj, a_class):
+    """a function that returns True if the object is\
+    an instance of a class that inherited\
+    (directly or indirectly) from the specified class;\
+    otherwise False."""
+    if isinstance(obj, a_class) and type(obj) != a_class:
+        return True
+    else:
+        return False
