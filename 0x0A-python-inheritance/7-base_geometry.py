07fcdfbbc5ca6e3365ee91092f1817458a341987
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-07-05 00:19:48 +0300

    no

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -12,7 +12,6 @@ class BaseGeometry:
 
     def integer_validator(self, name, value):
         """A method to validate the value entered as proper"""
-        """Validates if a name has the proper value"""
         if type(value) is not int:
             raise TypeError('{} must be an integer'.format(name))
         if value <= 0:
