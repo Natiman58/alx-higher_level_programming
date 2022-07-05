8649ca1a0fed3f6d429bafa0eb9a16004340cf24
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-07-05 22:53:45 +0300

    all

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def append_after(filename="", search_string="", new_string=""):
+    """
+        Inserts a new line of text to a file
+        after each line containing a specific string.
+     """
+    with open(filename, 'r+', encoding="utf-8") as f:
+        all_lines = f.readlines()
+
+    count = 0
+    with open(filename, 'w', encoding="utf-8") as j:
+        for line in all_lines:
+            count += 1
+            if search_string in line:
+                all_lines.insert(count, new_string)
+        for line in all_lines:
+            j.write(line)
