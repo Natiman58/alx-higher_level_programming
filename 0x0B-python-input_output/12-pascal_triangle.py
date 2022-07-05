5bebbbabdf1245064ab31f6c714575d37f869444
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-07-05 22:35:01 +0300

    final

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    """
+        A function that returns a list of integers representing
+        pascal's triangle of n
+    """
+    if n <= 0:
+        return []
+    lis_t = []
+    for i in range(n):
+        if i == 0:
+            lis_t.append([1])
+            continue
+        if i == 1:
+            lis_t.append([1, 1])
+            continue
+        row = []
+        for item in range(i + 1):
+            row.append(item)
+        for item in range(1, i):
+            row[0] = 1
+            row[i] = 1
+            row[item] = lis_t[i - 1][item] + lis_t[i - 1][item - 1]
+        lis_t.append(row)
+    return lis_t
