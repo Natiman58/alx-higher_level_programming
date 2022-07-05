eedd557351c91db0cf5264f2df877a3c6cff1034
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-07-05 22:57:42 +0300

    \

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -4,7 +4,7 @@
 """
 def append_after(filename="", search_string="", new_string=""):
     """
-        Inserts a new line of text to a file\
+        Inserts a new line of text to a file
         after each line containing a specific string.
      """
     with open(filename, 'r+', encoding="utf-8") as f:
