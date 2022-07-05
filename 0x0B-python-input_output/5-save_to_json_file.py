4bb008832d3e0c3a811a4060a01add74b13c8f23
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-07-05 16:31:10 +0300

    dump

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+    A module that contains the function
+    that writes an object to a text file
+    using JSON representation.
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """writes 'my_obj' to a text file 'filename'
+        in the form of JSON.
+    """
+    with open(filename, "w") as f:
+        return json.dump(my_obj, f)
