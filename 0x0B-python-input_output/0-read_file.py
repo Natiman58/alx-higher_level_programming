00545ef83cafd76d08faf4fecff1737b7fef3f00
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-07-05 12:44:06 +0300

    r

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+    A module that contains the 'read_file' function
+"""
+
 
 def read_file(filename=""):
     """A function that reads a text file(UTF8) and prints it to stdout"""
