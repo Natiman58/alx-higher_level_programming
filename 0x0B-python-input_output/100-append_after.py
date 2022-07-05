dc8566037b3bc154a81651a214cf0e56da1d45a9
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-07-05 22:55:08 +0300

    com

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""
+    A module containing the 'append_after' fun.
+"""
 def append_after(filename="", search_string="", new_string=""):
     """
-        Inserts a new line of text to a file
+        Inserts a new line of text to a file\
         after each line containing a specific string.
      """
     with open(filename, 'r+', encoding="utf-8") as f:
