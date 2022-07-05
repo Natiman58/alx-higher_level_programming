50020714b73f1db9ad7ea731a5ef40944ff6b2cc
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-07-05 21:51:43 +0300

    j

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+    A module that contains the class Student
+"""
+
+class Student:
+    """
+        A class that defines a student by
+        first_name, last_name, age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize the attributes. """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+            Retrieves the dictionary representation of Student class\
+            if the attr is in attrs retrieve only the name if not all
+        """
+        if type(attrs) != list:
+            return self.__dict__
+        else:
+            temp = {}
+            for i in attrs:
+                if type(i) != str:
+                    return self.__dict__
+                if i in self.__dict__.keys():
+                    temp[i] = self.__dict__[i]
+            return temp
+
+    def reload_from_json(self, json):
+        """
+            Replaces all attributes of the student class.
+            json: a dictionary of attributes
+        """
+        for i in json:
+            self.__dict__.update({i: json[i]})
