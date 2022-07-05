a763e79a3b345605dd0d50c21612584041d6525f
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-07-05 20:20:29 +0300

    dictr

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,20 +1,23 @@
 #!/usr/bin/python3
 """
-    A module containing a student class.
+    A module that contains the class Student
 """
 
 
 class Student:
     """
-        A class that defines a student by
-        first_name, last_name, age
+        A class that defines a student by\
+        first_name, last_name, age.
     """
-    def __int__(self, first_name, last_name, age):
-        """Initialize the attributes"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize the attributes."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Retrieves the dictionary representation of a student class."""
+        """
+            Retrieves the dictionary form of Student class.
+        """
         return self.__dict__
