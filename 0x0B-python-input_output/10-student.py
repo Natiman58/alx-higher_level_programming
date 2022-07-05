10fef5a8dda75ff5ace8b7266edb1f5063a05a66
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-07-05 21:26:42 +0300

    init

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -10,7 +10,6 @@ class Student:
     """
 
     def __init__(self, first_name, last_name, age):
-        """Initialize the attributes."""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
