2d7f20908daa8933d082e7d2f89a9579e1d74911
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-07-07 12:46:55 +0300

    models

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    A module containing the class 'Base'\
+    This class will be the “base” of all\
+    other classes in this project.\
+    The goal of it is to manage id attribute\
+    in all your future classes and to avoid\
+    duplicating the same code (by extension, same bugs).
+"""
+
+
+class Base:
+    """
+        A base class with private class attribute '__nb_objects'
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+            Initialize the attributes.
+            arg:
+                id - argument value of id
+                self. id - public instance attribute id
+        """
+        if id is not None and id is int:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
