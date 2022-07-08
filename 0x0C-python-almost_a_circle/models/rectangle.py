f33da24001e1fbf45c2b623ffe3858077edc853d
Natiman58 <99422296+Natiman58@users.noreply.github.com>
2022-07-08 13:23:38 +0300

    add

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -93,6 +93,16 @@ class Rectangle(Base):
 
     def update(self, *args):
         """Assigns an argument for each attribute."""
-
-
-
+        if len(args) != 0 and args is not None:
+            if len(args) >= 1:
+                if type(args[0]) != int and args[0] is not None:
+                    raise TypeError("id must be an integer")
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
