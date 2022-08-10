a5191f4c9806fd17347dd0a8522717d474573660
natiman58 <natiman58@gmail.com>
2022-08-10 09:37:57 +0300

    ALL Privileges

---
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+
+-- All the previleges for user_od_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- All the previleges for user_od_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
