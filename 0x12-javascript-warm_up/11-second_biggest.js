e1aec1d3ce0e2515eaf9ae3f96642128fac2df3d
Natiman58 <natiman58@gmail.com>
2022-09-05 09:07:48 +0300

    smi

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
-let array = (process.argv).slice(2).map(Number);
-let sortedArray = array.sort(function (a, b) { return a - b; });
 
-if (array.length <= 3) {
+if (process.argv.length <= 3) {
   console.log('0');
-} else { console.log(sortedArray[4]); }
+} else {
+  const array = (process.argv).slice(2).map(Number);
+  const sortedArray = array.sort(function (a, b) { return a - b; });
+  console.log(sortedArray[4]);
+}
