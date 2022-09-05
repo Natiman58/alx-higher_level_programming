e8bbdcd5134cb836be473c6a4134b71ed0a71c37
Natiman58 <natiman58@gmail.com>
2022-09-05 09:09:18 +0300

    b-a

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,6 @@ if (process.argv.length <= 3) {
   console.log('0');
 } else {
   const array = (process.argv).slice(2).map(Number);
-  const sortedArray = array.sort(function (a, b) { return a - b; });
-  console.log(sortedArray[4]);
+  const sortedArray = array.sort(function (a, b) { return b - a; });
+  console.log(sortedArray[1]);
 }
