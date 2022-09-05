496388d450c59322b96eba618283b7ed40cdb0e8
Natiman58 <natiman58@gmail.com>
2022-09-05 09:00:10 +0300

    some

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,8 +3,6 @@ const num = process.argv[2];
 const array = (process.argv).slice(2).map(Number);
 const sortedArray = array.sort(function (a, b) { return a - b; });
 
-if (num === undefined || num === '1') {
+if (array.length <= 3) {
   console.log('0');
-} else {
-  console.log(sortedArray[4]);
-}
+} else { console.log(sortedArray[4]); }
