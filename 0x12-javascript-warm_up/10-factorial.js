2f406412370c751f4c85a9366dccee0d5a682935
Natiman58 <natiman58@gmail.com>
2022-09-05 07:46:50 +0300

    semis

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,13 +1,10 @@
 #!/usr/bin/node
 const x = process.argv[2];
 
-if (x === undefined || isNaN(x)) {
-  console.log(parseInt('1'));
-} else {
-  function factorial (x) {
-    if (x === 0 || x === 1) {
-      return 1;
-    } else { return (x * factorial(x - 1)); }
-  }
-  console.log(factorial(Number(x)));
+function factorial (x) {
+  if (x === 0 || x === 1 || x === undefined || isNaN(x)) {
+    return 1;
+  } else { return (x * factorial(x - 1)); }
 }
+
+console.log(factorial(Number(x)));
