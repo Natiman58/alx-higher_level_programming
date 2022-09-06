525ee076afc19cd4396e7c32b59f703e3ae57179
Natiman58 <natiman58@gmail.com>
2022-09-06 20:49:36 +0300

    const

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (((w = parseInt(w)) > 0) && ((h = parseInt(h)) > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print (shape = 'X') {
+    for (let i = 0; i < this.height; i++) {
+      console.log(shape.repeat(this.width));
+    }
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+}
+module.exports = Rectangle;
