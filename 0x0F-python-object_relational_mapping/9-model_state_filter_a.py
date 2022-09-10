91a9f5a1737672cdb7dea8c75a7a11bc651d5494
natiman58 <natiman58@gmail.com>
2022-09-10 21:38:17 +0300

    like%a%

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+    script that lists all State objects that contain
+    the letter 'a' from the database hbtn_0e_6_usa
+"""
+
+if __name__ == '__main__':
+    from model_state import Base, State
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+
+    user = sys.argv[1]
+    pw = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(user, pw, db_name, pool_pre_ping=True))
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+
+    state_a = session.query(State).order_by(State.id).filter(State.name.like('%a%'))
+
+    if state_a:
+        for state in state_a:
+            print("{}:{}".format(state.id, state.name))
+
+    session.close()
+
