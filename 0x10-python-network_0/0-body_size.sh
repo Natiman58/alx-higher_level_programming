21b6919b29caddbf9bfc0acaf94caa60978cc0c9
Natiman58 <natiman58@gmail.com>
2022-09-22 13:44:01 +0000

    final

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # A bash script that takes in a URL and sends the request to that URL
 # and displays the size if the body of the response
-curl -Is "$1" | grep 'Content-Length' | awk '{print $2}'
+curl -sI xdg-open "$1" | grep -i Content-Length | awk '{print $2}'
 
