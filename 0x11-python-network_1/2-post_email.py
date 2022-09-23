79ed4852ea482ab54be52d12342e4410d0c64082
Natiman58 <natiman58@gmail.com>
2022-09-23 01:03:19 -0700

    pyp

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
+"""
+    A python script that sends a POST request to URL for an email
+"""
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    import sys
 
+    url = sys.argv[1]
+    param = {"email": sys.argv[2]}
+    query_string = urllib.parse.urlencode(param)
+    data = query_string.encode("ascii")
+
+    with urllib.request.urlopen(url, data) as response:
+        response_text = response.read().decode("utf-8")
+        print(response_text)
