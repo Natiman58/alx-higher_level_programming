5092fa1bb3938e3f6960c5e7409a3774ad4c9227
Natiman58 <natiman58@gmail.com>
2022-10-26 22:35:09 +0300

    get

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,8 +1,8 @@
 const $ = window.$;
 
 $(function () {
-  $.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (jsonData, textStatus) {
-    if (textStatus === 'success') {
+  $.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (jsonData, requestStatus) {
+    if (requestStatus === 'success') {
       $('#character').text(jsonData.name);
     }
   });
